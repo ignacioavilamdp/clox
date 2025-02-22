db341b3016690a49c28ad7432edf4a29e74db1c2
Ignacio Avila <ignacioavilamdp@gmail.com>
2025-02-22 19:30:05 -0300

    A Virtual Machine added

--- makefile
+++ makefile
@@ -10,7 +10,7 @@ CFLAGS = -Wall
 TARGET_EXEC = main.exe
 
 # Source files
-SRC_FILES := chunk.c memory.c main.c debug.c value.c
+SRC_FILES := chunk.c memory.c main.c debug.c value.c vm.c
 
 # Object files
 OBJS = $(patsubst %.c, $(BUILD_DIR)/%.o, $(SRC_FILES))
--- src/main.c
+++ src/main.c
@@ -2,20 +2,45 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int arg, const char* argv[]){
-    Chunk chunk;
+    initVM();
 
+    Chunk chunk;
     initChunk(&chunk);
 
+    // 1.2 constant
     int constant = addConstant(&chunk, 1.2);
     writeChunk(&chunk, OP_CONSTANT, 123);
     writeChunk(&chunk, constant, 123);
 
-    writeChunk(&chunk, OP_RETURN, 123);
+    // 3.4 constant
+    constant = addConstant(&chunk, 3.4);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    // sum
+    writeChunk(&chunk, OP_ADD, 123);
+
+    // 5.6 constant
+    constant = addConstant(&chunk, 5.6);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+    
+    // divide
+    writeChunk(&chunk, OP_DIVIDE, 123);
+    
+    // negate
+    writeChunk(&chunk, OP_NEGATE, 123);
+    
+    // return
     writeChunk(&chunk, OP_RETURN, 123);
 
+    // disassemble and interpret
     disassembleChunk(&chunk, "test chunk");
+    interpret(&chunk);
+    freeVM();
     freeChunk(&chunk);
 
     return 0;
